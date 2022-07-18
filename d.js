722e11f1663dc56b671af33414273606626f1fb2
sanaycode <88113041+sanaycode@users.noreply.github.com>
2022-07-18 13:08:51 -0400

    Add files via upload

---
+++ d.js
@@ -0,0 +1,37 @@
+function back(){
+    window.location="index.html";
+}
+function preload(){
+    img=loadImage("desk.jpg")
+}
+function setup(){
+    canvas=createCanvas(640, 420);
+    canvas.position(450, 260);
+    document.getElementById("status").innerHTML="Status : Detecting Object";
+}
+function modelLoaded(){
+    console.log("Model Loaded!");
+    objectDetector.detect(img, gotResults);
+    status="true";
+}
+function gotResults(error, results){
+    if(error){
+        console.log(error);
+    }
+    console.log(results);
+    objects=results;
+}
+function draw(){
+    image(img, 0, 0, 640, 420);
+    if(status != ""){
+        for(i=0, i;objects.length(); i++){
+            document.getElementById("status").innerHTML="Status: Object Detected";
+            fill("#FF0000");
+            percent=floor(objects[i].confidence*100);
+            text(objects[i].label+""+percent+"%"+objects[i].x+15+objects[i].y+15);
+            noFill();
+            stroke("#FF0000");
+            rect(objects[i].x, objects[i].y, objects[i].width, objects[i].height);
+        }
+    }
+}
---
+++ main.js
@@ -0,0 +1,15 @@
+function bpage(){
+    window.location="bedroom.html";
+}
+function tapage(){
+    window.location="tvandac.html";
+}
+function dpage(){
+    window.location="desk.html";
+}
+function btpage(){
+    window.location="bottles.html";
+}
+function fbpage(){
+    window.location="fruitbasket.html";
+}
